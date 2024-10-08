90801d46b60dcc1ef5a8795b0a7c06aa501aba36
Sphiwe <117762641+SphiweGM@users.noreply.github.com>
2024-10-08 19:37:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { neon } = require("@neondatabase/serverless");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 // const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +11,7 @@ const sql = neon(
 );
 
 // use the express-static middleware
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
