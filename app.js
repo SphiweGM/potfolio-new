07d5feabd632e16dbd021d0e40ae6ee8a5e6d4bd
Sphiwe <117762641+SphiweGM@users.noreply.github.com>
2024-10-09 09:34:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const sql = neon(
 // use the express-static middleware
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // define the first route
 app.get("/", async (req, res) => {
